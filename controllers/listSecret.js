3d9fce66e22db0caf3f383a29a64ef3d79a809ec
poojithullasa <upsy.0515@gmail.com>
2021-10-07 19:02:03 +0530

    Added functions to view and list secrets

--- controllers/listSecret.js
+++ controllers/listSecret.js
@@ -1,3 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+const outputMessage = require("../constants/output");
+
 exports.listSecret = (request, response) => {
-  response.send("/list api secret called");
+  const location = path.join("/", "Vault", "/");
+  const files = this.getFiles(location);
+  const secrets = this.getSecrets(location);
+  if (files.length == undefined || secrets.length == undefined) {
+    const output = outputMessage.failureResponse;
+    output.nodes = files;
+    output.secrets = secrets;
+    output.route = "/secret/list";
+    response.send(output);
+  } else {
+    const output = outputMessage.successResponse;
+    files.forEach((file) => {
+      output.result.push({ entityType: "node", name: file });
+    });
+    secrets.forEach((secret) => {
+      output.result.push({ entityType: "secret", name: secret });
+    });
+    response.send(output);
+  }
+};
+
+exports.getFiles = (path) => {
+  try {
+    const files = fs
+      .readdirSync(path, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
+    return files;
+  } catch (error) {
+    return error;
+  }
+};
+
+exports.getSecrets = (path) => {
+  try {
+    const secrets = fs
+      .readdirSync(path, { withFileTypes: true })
+      .filter((dirent) => dirent.isFile())
+      .map((dirent) => dirent.name);
+    return secrets;
+  } catch (error) {
+    return error;
+  }
 };
--- controllers/viewSecret.js
+++ controllers/viewSecret.js
@@ -1,3 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const secretResponse = require("../constants/output");
+
 exports.viewSecret = (request, response) => {
-  response.send("/view api secret called");
+  const location = path.join("/", "Vault", "/Secret4");
+  const values = this.readFile(location);
+  if (values.length == undefined) {
+    const output = secretResponse.errorResponse;
+    output.route = "/secret/view";
+    output.secret = values;
+    response.send(output);
+  } else {
+    const output = secretResponse.secretResponse;
+    output.value.push(values);
+    response.send(output);
+  }
+};
+
+exports.readFile = (path) => {
+  try {
+    const data = fs.readFileSync(path, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return error;
+  }
 };
