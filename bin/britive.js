a9bab5d75ef6e9181fc673a71238f7a1a3a4087d
poojithullasa <upsy.0515@gmail.com>
2021-10-08 12:58:26 +0530

    bug fix: Sub Command

--- bin/britive.js
+++ bin/britive.js
@@ -4,6 +4,5 @@ const program = new Command();
 
 program
   .version("1.0.0")
-  .command("secret")
-  .description("This commands helps us to view and list secrets")
+  .command("secret", "This commands helps us to view and list secrets")
   .parse(process.argv);
