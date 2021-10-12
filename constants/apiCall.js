b86c964ad6b105879099d11611e8f6f9d1ae23a3
poojithullasa <upsy.0515@gmail.com>
2021-10-12 18:17:28 +0530

    Added post and get call to britive

--- constants/apiCall.js
+++ constants/apiCall.js
@@ -1,20 +1,42 @@
 import axios from "axios";
 
-export const apiCall = async (url, path, vault) => {
+export const getApiCall = async (vault, path, type) => {
   const response = await axios({
     method: "GET",
-    baseURL: "http://localhost:3000",
-    url: url,
+    baseURL: `https://secmgr1.dev.britive-app.com/api/v1/secretmanager/${vault}/secrets`,
     params: {
       path: path,
-      vault: vault,
+      type: type,
+    },
+    headers: {
+      Authorization: `TOKEN ${process.env.TOKEN}`,
     },
   })
     .then(function (response) {
       return response.data;
     })
     .catch(function (error) {
-      return error.response.status;
+      return error;
+    });
+  return response;
+};
+
+export const postApiCall = async (path, vault) => {
+  const response = await axios({
+    method: "POST",
+    baseURL: `https://secmgr1.dev.britive-app.com/api/v1/secretmanager/${vault}/accesssecrets`,
+    params: {
+      path: path,
+    },
+    headers: {
+      Authorization: `TOKEN ${process.env.TOKEN}`,
+    },
+  })
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      return error;
     });
   return response;
 };
